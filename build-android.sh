5b7bf232c910525d12924bf043b2925ca955f701
John Wason <wason@wasontech.com>
2023-12-30 23:06:42 -0500

    fix url

--- build-android.sh
+++ build-android.sh
@@ -240,7 +240,7 @@ echo "Building boost version: $BOOST_VER1.$BOOST_VER2.$BOOST_VER3"
 # Build constants
 # -----------------------
 
-BOOST_DOWNLOAD_LINK=https://github.com/boostorg/boost/releases/download/$BOOST_VER1.$BOOST_VER2.$BOOST_VER3/boost-$BOOST_VER1.$BOOST_VER2.$BOOST_VER3.tar.gz
+BOOST_DOWNLOAD_LINK=https://github.com/boostorg/boost/releases/download/boost-$BOOST_VER1.$BOOST_VER2.$BOOST_VER3/boost-$BOOST_VER1.$BOOST_VER2.$BOOST_VER3.tar.gz
 #BOOST_DOWNLOAD_LINK="https://boostorg.jfrog.io/artifactory/main/release/$BOOST_VER1.$BOOST_VER2.$BOOST_VER3/source/boost_${BOOST_VER1}_${BOOST_VER2}_${BOOST_VER3}.tar.bz2"
 BOOST_TAR="boost_${BOOST_VER1}.${BOOST_VER2}.${BOOST_VER3}.tar.gz"
 BOOST_DIR="boost_${BOOST_VER1}.${BOOST_VER2}.${BOOST_VER3}"
